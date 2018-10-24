1735939f23577af586190716577f5f2d8576cacf
Taru Hakuni <taru.hakuni@gmail.com>
2018-10-24 21:54:56 +0300

    Improve movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,9 +11,33 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 movement = new Vector3();
+
 		if (Input.GetKey(KeyCode.DownArrow))
 		{
-			transform.Translate(Vector3.forward * Time.deltaTime);
+			movement += Vector3.back;
 		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			movement += Vector3.forward;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			movement += Vector3.left;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			movement += Vector3.right;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			movement += Vector3.up;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			movement += Vector3.down;
+		}
+		transform.position += movement * Time.deltaTime;
 	}
 }
+
